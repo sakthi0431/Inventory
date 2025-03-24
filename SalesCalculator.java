e8cd9b26f545934a45ca8b6618d01229da906846
sakthi0431 <sakthib440@gmail.com>
2025-03-24 09:00:48 +0530

    SalesCalculator.java

---
+++ SalesCalculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class SalesCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of items sold: ");
+        int numberOfItems = scanner.nextInt();
+
+        double totalSales = 0;
+
+        for (int i = 1; i <= numberOfItems; i++) {
+            System.out.println("Enter the price of item " + i + ": ");
+            double price = scanner.nextDouble();
+
+            System.out.println("Enter the quantity of item " + i + ": ");
+            int quantity = scanner.nextInt();
+
+            totalSales += price * quantity;
+        }
+
+        System.out.println("Total sales amount: $" + totalSales);
+
+        scanner.close();
+    }
+}
